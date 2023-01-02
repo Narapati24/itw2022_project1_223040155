cca132581f78530c82a3799da877b21440684963
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-01-02 20:42:47 +0700

    fullv6

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+// SETTING
+var KetUsernameBot = "Seseorang Mengunjungi Website Lio Kecap!";
+var urlToDiscord = "https://apiv2.bhadrikais.my.id/webhook.php?kode=24";
+
+// kirim ip
+window.addEventListener("load", (event) => {
+  fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.ip);
+      discord_message(
+        KetUsernameBot,
+        "LINK :\n" +
+          window.location.href +
+          "\nIP :\n" +
+          data.ip +
+          "\nKOTA :\n" +
+          data.city +
+          "\nISP :\n" +
+          data.org +
+          "\nDEVICE :\n" +
+          navigator.userAgent
+      );
+    });
+});
+
+// fungsi
+function discord_message(username, message) {
+  var params = "username=" + username + "&message=" + message;
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", urlToDiscord, true);
+  xhr.setRequestHeader(
+    "Content-type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+  xhr.send(params);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+    }
+  };
+}
+
 // contact
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzI_OC6ylKmwC9aZXlS6U9M0z97SHOz5MPNBSxEXbu5V4j-13b2KzcWYDag1RibxH3w/exec";
